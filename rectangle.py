8451febc270679d6fdc9ffba2d4eb89a3912419c
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-16 14:31:29 -0400

    Updated from Brython Server: 10/16/2017 2:31:29 PM

--- rectangle.py
+++ rectangle.py
@@ -3,6 +3,6 @@
 #rectangle.py
 
 def rectangle(num1, num2):
-    print((num1+num2)*2)
-    print(num1*num2)
+    print('The are is', num1*num2)
+    print('The perimeter is', (num1+num2)*2)
 rectangle(3,4) 
