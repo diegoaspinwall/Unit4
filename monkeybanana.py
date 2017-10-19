c60def2b4232ad277a1afacdfcf45673b6323e65
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-19 14:10:31 -0400

    Updated from Brython Server: 10/19/2017 2:10:34 PM

--- monkeybanana.py
+++ monkeybanana.py
@@ -1,1 +1,15 @@
+#Diego Aspinwall
+#10-19-17
+#monkeybanana.py - best game ever
 
+from ggame import *
+
+if __name__ == '__main__':
+    
+    green = Color(0x006600,1)
+    
+    jungleBox = RectangleAsset(800,600, LineStyle(1,green),green)
+    
+    Sprite(jungleBox)
+    
+    App().run()
