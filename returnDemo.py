701687c479a60b03786fd83964cdfe3ec21dc3a6
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-19 15:50:05 -0400

    Updated from Brython Server: 10/19/2017 3:50:08 PM

--- returnDemo.py
+++ returnDemo.py
@@ -1,14 +1,14 @@
 #Sam	Smedinghoff
 #10/18/17
-#returnDemo.py	- how	to	use	return	with	a	function
+#returnDemo.py	- how to use return with a function
 from	random	import	randint
 def	randevenint(low,high):
 	num = randint(low,high)
-	while num%2	==	1:	#while	the	number	is	odd
-		num	= randint(low,high)
+	while num%2 == 1:	#while the number is odd
+		num = randint(low,high)
 	return num
 
-r1	= randevenint(1,100)
-r2	= randevenint(1,100)
-r3	= randevenint(1,100)
+r1 = randevenint(1,100)
+r2 = randevenint(1,100)
+r3 = randevenint(1,100)
 print(r1, r2, r3)
