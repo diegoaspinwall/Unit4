9ee8caa539b4d773aaadfaa5b3f32b14e8e4d483
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-20 14:55:14 -0400

    Updated from Brython Server: 10/20/2017 2:55:15 PM

--- stringUnion.py
+++ stringUnion.py
@@ -1,11 +1,12 @@
 #Diego Aspinwall
 #10-20-17
 #stringUnion.py
-
+"""
 def stringUnion(word1,word2):
     ans = ''
     for ch in word1:
-        
-print(word1+word2)
+"""
+
+print('word1'+'word2')
 
 
