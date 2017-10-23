a5c5dd2fc625f23d8e4eb7dce8ed2048b6245fae
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-23 14:17:47 -0400

    Updated from Brython Server: 10/23/2017 2:17:48 PM

--- localDemo.py
+++ localDemo.py
@@ -1,1 +1,12 @@
+#Diego Aspinwall
+#10-23-17
+#localDemo.py - understanding local variables
 
+def f():
+    x = 77 #x is a loval variable
+    y = 10 #y is also a local variable
+
+x = 5
+f() #x does not change
+print(x)
+print(y)
