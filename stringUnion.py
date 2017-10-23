1a59851e6ee82d4f72d6d44c47c8cdc9a446eef5
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-22 22:23:39 -0400

    Updated from Brython Server: 10/22/2017 10:23:37 PM

--- stringUnion.py
+++ stringUnion.py
@@ -1,12 +1,9 @@
 #Diego Aspinwall
 #10-20-17
 #stringUnion.py
-"""
+
 def stringUnion(word1,word2):
     ans = ''
-    for ch in word1:
-"""
-
-print('word1'+'word2')
-
-
+    megaword = word1+word2
+    for ch in megaword:
+        
