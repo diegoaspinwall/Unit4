387f0c7f78623c3ba81347118fc5b9e20a805031
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-22 22:39:33 -0400

    Updated from Brython Server: 10/22/2017 10:39:31 PM

--- stringIntersect.py
+++ stringIntersect.py
@@ -1,1 +1,12 @@
+#Diego Aspinwall
+#10-22-17
+#stringIntersect.py
 
+def stringIntersect(word1,word2):
+    ans = ''
+    for ch in word1:
+        if ch in word2:
+            ans += ch
+    print(ans)
+
+stringIntersect('Mississippi','Pennsylvania')
