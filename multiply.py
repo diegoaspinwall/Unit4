ff00d3ff0d41335b9d2c46780c90425f4f097c47
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-24 11:14:06 -0400

    Updated from Brython Server: 10/24/2017 11:14:02 AM

--- multiply.py
+++ multiply.py
@@ -4,11 +4,17 @@
 
 from random import randint
 
+nicejob():
+    
+
 total = 0
 while total<5:
     num1 = randint(1,12)
     num2 = randint(1,12)
     guess = int(input(num1, ' x ', num2,' = '))
     if num1*num2 == guess:
-        
+        total += 1
+        nicejob()
+    else:
+        print('Sorry, incorrect')
 
