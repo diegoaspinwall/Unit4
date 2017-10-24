875d71cdec3f424748e92abb59e7aea4dfb74d92
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-24 15:16:12 -0400

    Updated from Brython Server: 10/24/2017 3:16:12 PM

--- bubbles.py
+++ bubbles.py
@@ -4,29 +4,7 @@
 
 from random import randint
 from ggame import *
-"""
-def mouseClick(event):
-    numcolor = randint(1,4)
-    if numcolor==1:
-        red = Color(0xff0000, 1)
-        line = LineStyle(3,red)
-        circle = CircleAsset(,line,red)
-    elif numcolor==2:
-        blue = Color(0x0000ff, 1)
-        line = LineStyle(3,blue)
-        circle = CircleAsset(1000,1000,line,blue)
-    elif numcolor==3:
-        yellow = Color(0xffff00, 1)
-        line = LineStyle(3,yellow)
-        circle = CircleAsset(1000,1000,line,yellow)
-    elif numcolor==4:
-        green = Color(0x00ff00, 1)
-        line = LineStyle(3,green)
-        circle = CircleAsset(1000,1000,line,green)
-    Sprite(rectangle)
-App().listenMouseEvent('click', mouseClick)
-App().run()
-"""
+
 def mouseClick(event):
     numcolor = randint(1,4)
     if numcolor==1:
