5ab3a89a377009cb4732611ffe08b2730e49f1c6
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-25 13:51:56 -0400

    Updated from Brython Server: 10/25/2017 1:51:59 PM

--- bouncingBall.py
+++ bouncingBall.py
@@ -14,13 +14,13 @@ def step():
         moveCircle()
 
 if _name_ == '_main_':
-
-black = Color(0x000000,1)
-green = Color(0x00FF00,1)
-
-greenCircle = CircleAsset(50,LineStyle(1,green),green)
-blackRectangle = RectangleAsset(1000,600,LineStyle(1,black),black)
-
-Sprite(blackRectangle)
-ball = Sprite(greenCircle, (50,50))
-App().run()
+    
+    black = Color(0x000000,1)
+    green = Color(0x00FF00,1)
+    
+    greenCircle = CircleAsset(50,LineStyle(1,green),green)
+    blackRectangle = RectangleAsset(1000,600,LineStyle(1,black),black)
+    
+    Sprite(blackRectangle)
+    ball = Sprite(greenCircle, (50,50))
+    App().run()
