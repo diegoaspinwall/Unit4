ebc3e3054ed6b7c8ca4fb69c505731eeece1484f
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-25 13:51:21 -0400

    Updated from Brython Server: 10/25/2017 1:51:23 PM

--- bouncingBall.py
+++ bouncingBall.py
@@ -3,6 +3,15 @@
 #bouncingBall.py
 
 from ggame import *
+def moveCircle():
+    circle.x = circle.x+1
+    cicle.y = circle.y+1
+    data['frames'] = 0
+
+def step():
+    data['frames'] += 1
+    if data['frames'] == 1:
+        moveCircle()
 
 if _name_ == '_main_'
 
@@ -11,11 +20,6 @@ green = Color(0x00FF00,1)
 
 greenCircle = CircleAsset(50,LineStyle(1,green),green)
 blackRectangle = RectangleAsset(1000,600,LineStyle(1,black),black)
-
-def moveCircle():
-    circle.x = randint(0,COLS-1)*CELL_SIZE
-    cicle.y = randint(0,ROWS-1)*CELL_SIZE
-    data['frames'] = 0
 
 Sprite(blackRectangle)
 ball = Sprite(greenCircle, (50,50))
