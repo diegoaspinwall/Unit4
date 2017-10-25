cb54afcad7bc88b67bad8db0c9b24582bc74be32
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-25 13:39:19 -0400

    Updated from Brython Server: 10/25/2017 1:39:21 PM

--- bouncingBall.py
+++ bouncingBall.py
@@ -4,4 +4,8 @@
 
 from ggame import *
 
+black = Color(0x000000,1)
+green = Color(0x00FF00,1)
 
+greenCircle = CircleAsset(50,LineStyle(1,green),green)
+greenRectangle = RectangleAsset(200,100,LineStyle(1,green),green)
