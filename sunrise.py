e2d2a8157aa994f030c30d131119b67fc2783192
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-26 14:19:18 -0400

    Updated from Brython Server: 10/26/2017 2:19:19 PM

--- sunrise.py
+++ sunrise.py
@@ -1,1 +1,21 @@
+#Diego Aspinwall
+#sunrise.py
+#10-26-17
+
+from ggame import *
+def moveCircle():
+    
+    circle.y = circle.y - 1
+
+if __name__ == '__main__':
+
+    black = Color(0x000000,1)
+    green = Color(0x0000FF,1)
+    
+    greenCircle = CircleAsset(50,LineStyle(1,green),green)
+    blackRectangle = RectangleAsset(1000,600,LineStyle(1,black),black)
+    
+    Sprite(blackRectangle)
+    circle = Sprite(greenCircle, (500,600))
+    App().run(moveCircle)
 
