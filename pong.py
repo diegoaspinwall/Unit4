338a6cf65d31f2c84997f2a8e8e39ffa41fcecbc
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-27 14:29:48 -0400

    Updated from Brython Server: 10/27/2017 2:29:47 PM

--- pong.py
+++ pong.py
@@ -6,15 +6,16 @@ from ggame import *
 
 X_LENGTH = 1000
 Y_LENGTH = 600
+CIRCLE_R = 50
 
 def moveCircle():
     
     circle.x = circle.x+data['directionx']
     circle.y = circle.y+data['directiony']
     
-    if circle.y == 50 or circle.y == Y_LENGTH-50:
+    if circle.y == CIRCLE_R or circle.y == Y_LENGTH-CIRCLE_R:
         data['directiony'] = -1*data['directiony']
-    if circle.x == 50 or circle.x == X_LENGTH-50:
+    if circle.x == CIRCLE_R or circle.x == X_LENGTH-CIRCLE_R:
         data['directionx'] = -1*data['directionx']
 
 if __name__ == '__main__':
@@ -26,9 +27,9 @@ if __name__ == '__main__':
     black = Color(0x000000,1)
     green = Color(0x0000FF,1)
     
-    greenCircle = CircleAsset(50,LineStyle(1,green),green)
+    greenCircle = CircleAsset(CIRCLE_R,LineStyle(1,green),green)
     blackRectangle = RectangleAsset(X_LENGTH,Y_LENGTH,LineStyle(1,black),black)
     
     Sprite(blackRectangle)
-    circle = Sprite(greenCircle, (50,50))
+    circle = Sprite(greenCircle, (CIRCLE_R,CIRCLE_R))
     App().run(moveCircle)
