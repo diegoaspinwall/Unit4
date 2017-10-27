224976fef2772377ce38522f7c7249c4ef24fddf
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-27 14:24:56 -0400

    Updated from Brython Server: 10/27/2017 2:24:55 PM

--- warmup12.py
+++ warmup12.py
@@ -4,12 +4,12 @@
 
 def GCF(num1,num2):
     if num1>num2:
-        for i in range(num2,1,-1):
+        for i in range(num2,0,-1):
             if num2%i==0 and num1%i==0:
                 return i
     else:
-        for i in range(num1,1,-1):
+        for i in range(num1,0,-1):
             if num1%i==0 and num2%i==0:
                 return i
 
-print(GCF(10,15))
+print(GCF(14,7))
