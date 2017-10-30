7bd61a8550ebfb51626ad1cc9d93f3ed07d7f908
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-29 20:02:01 -0400

    Updated from Brython Server: 10/29/2017 8:02:00 PM

--- pong.py
+++ pong.py
@@ -38,6 +38,7 @@ def moveCircle():
         circle.y = CIRCLE_R+10
         data['directionx'] = 10
         data['directiony'] = 10
+        updateScore2()
     if circle.x==X_LENGTH-(CIRCLE_R+PADDLE_X) and paddle1.y<circle.y<paddle1.y+PADDLE_Y:
         data['directionx'] = -1*data['directionx']
     if circle.x == (X_LENGTH-CIRCLE_R):
@@ -45,6 +46,7 @@ def moveCircle():
         circle.y = CIRCLE_R+10
         data['directionx'] = 10
         data['directiony'] = 10
+        updateScore1()
 
 def updateScore1():
     data['score1'] += 1
