bd679190e8cafd67733455d80aa7830aa14457da
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-29 20:07:57 -0400

    Updated from Brython Server: 10/29/2017 8:07:56 PM

--- pong.py
+++ pong.py
@@ -51,7 +51,7 @@ def moveCircle():
 def updateScore1():
     data['score1'] += 1
     data['scoreText'].destroy()
-    scoreBox = TextAsset()str(data['score1'])+'|'+str(data['score2'])),fill=scorecolor, style='bold 30pt Times')
+    scoreBox = TextAsset((str(data['score1'])+'|'+str(data['score2'])),fill=scorecolor, style='bold 30pt Times')
     data['scoreText'] = Sprite(scoreBox)
 
 def updateScore2():
