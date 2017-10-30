bdff45d18241649b9ed66cece464021c05f07ba2
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-10-30 14:32:48 -0400

    Updated from Brython Server: 10/30/2017 2:32:49 PM

--- quiz4.py
+++ quiz4.py
@@ -1,1 +1,23 @@
+#Diego Aspinwall
+#10-30-17
+#quiz4.py
 
+def csia():
+    for i in range(0,5):
+        print('Computer Science is Awesome')
+
+def average(n1,n2,n3):
+    return (n1+n2+n3)/3
+
+def lastLetter(word):
+    for ch in word:
+        letter = ch
+    return ch
+
+def same(num1,num2):
+    if num1 == num2:
+        return True
+    else:
+        return False
+
+lastLetter('Diego')
